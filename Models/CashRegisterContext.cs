c2ef6f344a4b400a4cf65946c2f83e974f3a93a3
Juan <jcruz1703@gmail.com>
2022-07-20 20:59:00 -0500

    Primer Commit

---
+++ Models/CashRegisterContext.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace CashRegister.Models
+{
+
+    public class CashRegisterContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; } = null!;
+
+        public string DbPath { get; }
+
+        public CashRegisterContext(DbContextOptions<CashRegisterContext> options) : base(options)
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = Path.Join(path, "CashRegisterJC.db");
+            Console.WriteLine(DbPath);
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite($"data source={DbPath}; foreing keys = true");
+        }
+
+    }
+
+}
