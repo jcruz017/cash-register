5043b4f65b068f8088c6f2fa4cb39df256d79531
Juan <jcruz1703@gmail.com>
2022-07-29 21:01:00 -0500

    Se agrego variable de entorno

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddCors(options => options.AddDefaultPolicy(Policy=>
+{
+    Policy.WithOrigins(Environment.GetEnvironmentVariable("ALLOWED_ORIGENS")!.Split(",")).AllowAnyMethod().AllowAnyHeader();
+}));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
@@ -13,7 +17,7 @@ builder.Services.AddDbContext<CashRegisterContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || Boolean.Parse(Environment.GetEnvironmentVariable("ENABLE_SWAGGER")!))
 {
     app.UseSwagger();
     app.UseSwaggerUI();
